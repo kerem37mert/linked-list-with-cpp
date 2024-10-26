fab327d11603a9e43e557942e7bd9f542829d362
Kerem <kerem37mert@yahoo.com>
2024-10-26 17:38:16 +0300

    first commit

---
+++ build/CMakeFiles/linkedlist.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linkedlist.dir/src/main.cpp.o"
+  "CMakeFiles/linkedlist.dir/src/main.cpp.o.d"
+  "linkedlist"
+  "linkedlist.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/linkedlist.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
